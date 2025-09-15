c65b9799927e84465df2eccec1b9a2324209eedf
rohithroshan-create <rohithroshan6253@gmail.com>
2025-09-15 12:16:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -135,7 +135,7 @@ st.markdown("""
         border-left: 3px solid #4caf50;
         margin: 0.5rem 0;
     }
-""", unsafe_allow_html=True)
+, unsafe_allow_html=True)
 class EnhancedRecommendationEngine:
 def init(self):
 self.recommendations = {
